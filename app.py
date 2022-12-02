1e002ac27dd767588af14527770e76c1b8b66158
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2022-12-03 00:42:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -25,35 +26,25 @@ def user_access():
 
 @app.route("/currency", methods = ['GET', 'POST'])
 def currency_converter():
-    currency_list = [
-        {'bank': 'A1', 'date': "2022-11-25", 'currency': 'UAH', 'buy_rate': 0.025, 'sale_rate': 0.023},
-        {'bank': 'A1', 'date': "2022-11-25", 'currency': 'EUR', 'buy_rate': 0.9, 'sale_rate': 0.95},
-        {'bank': 'A1', 'date': "2022-11-25", 'currency': 'USD', 'buy_rate': 1, 'sale_rate': 1},
-        {'bank': 'A1', 'date': "2022-11-25", 'currency': 'GPB', 'buy_rate': 1.15, 'sale_rate': 1.2}
-    ]
+    con = sqlite3.connect("currencyData.db")
+    cursor = con.cursor()
 
     if request.method == 'POST':
         user_bank = request.form['bank']
         user_currency_1 = request.form['currency_1']
         user_date = request.form['date']
         user_currency_2 = request.form['currency_2']
-        buy_rate_1 = 0
-        buy_rate_2 = 0
-        sale_rate_1 = 0
-        sale_rate_2 = 0
-        for one_currency_info in currency_list:
-            if user_bank == one_currency_info['bank'] and user_currency_1 == one_currency_info['currency'] \
-                    and user_date == one_currency_info['date']:
-                buy_rate_1 = one_currency_info['buy_rate']
-                sale_rate_1 = one_currency_info['sale_rate']
-            if user_bank == one_currency_info['bank'] and user_currency_2 == one_currency_info['currency'] \
-                    and user_date == one_currency_info['date']:
-                buy_rate_2 = one_currency_info['buy_rate']
-                sale_rate_2 = one_currency_info['sale_rate']
 
-        cur_exchange_buy = buy_rate_2 / buy_rate_1
-        cur_exchange_sale = sale_rate_2 / sale_rate_1
+        res_1 = cursor.execute(f'SELECT buyRate, sellRate FROM currency WHERE bank="{user_bank}" and dateExchange="{user_date}" and currency="{user_currency_1}"')
+        buy_rate_1, sale_rate_1 = res_1.fetchone()
+        res_2 = cursor.execute(f'SELECT buyRate, sellRate FROM currency WHERE bank="{user_bank}" and dateExchange="{user_date}" and currency="{user_currency_2}"')
+        buy_rate_2, sale_rate_2 = res_2.fetchone()
 
+        cur_exchange_buy = round(buy_rate_2 / buy_rate_1, 2)
+        cur_exchange_sale = round(sale_rate_2 / sale_rate_1, 2)
+
+        cursor.close()
+        con.close()
         return render_template('currencyForm.html',
                                cur_exchange_buy = cur_exchange_buy,
                                cur_exchange_sale = cur_exchange_sale,

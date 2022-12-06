ec6695d9334f13390fb555652269c9f95271486f
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2022-12-06 21:28:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import request
+from dbFunc import DBManager
 import sqlite3
 
 app = Flask(__name__)
@@ -24,27 +25,25 @@ def register_user():
 def user_access():
     return "More functions"
 
+
+
 @app.route("/currency", methods = ['GET', 'POST'])
 def currency_converter():
-    con = sqlite3.connect("currencyData.db")
-    cursor = con.cursor()
-
     if request.method == 'POST':
+
+
         user_bank = request.form['bank']
         user_currency_1 = request.form['currency_1']
         user_date = request.form['date']
         user_currency_2 = request.form['currency_2']
 
-        res_1 = cursor.execute(f'SELECT buyRate, sellRate FROM currency WHERE bank="{user_bank}" and dateExchange="{user_date}" and currency="{user_currency_1}"')
-        buy_rate_1, sale_rate_1 = res_1.fetchone()
-        res_2 = cursor.execute(f'SELECT buyRate, sellRate FROM currency WHERE bank="{user_bank}" and dateExchange="{user_date}" and currency="{user_currency_2}"')
-        buy_rate_2, sale_rate_2 = res_2.fetchone()
+        with DBManager() as db:
+            buy_rate_1, sale_rate_1 = db.getData(f'SELECT buyRate, sellRate FROM currency WHERE bank="{user_bank}" and dateExchange="{user_date}" and currency="{user_currency_1}"')
+            buy_rate_2, sale_rate_2 = db.getData(f'SELECT buyRate, sellRate FROM currency WHERE bank="{user_bank}" and dateExchange="{user_date}" and currency="{user_currency_2}"')
 
         cur_exchange_buy = round(buy_rate_2 / buy_rate_1, 2)
         cur_exchange_sale = round(sale_rate_2 / sale_rate_1, 2)
 
-        cursor.close()
-        con.close()
         return render_template('currencyForm.html',
                                cur_exchange_buy = cur_exchange_buy,
                                cur_exchange_sale = cur_exchange_sale,

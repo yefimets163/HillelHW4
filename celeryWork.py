4f518cc8e58cb722669ab51e3731b2e3f53d379e
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2023-01-10 21:02:38 +0200

    Update celeryWork.py

--- celeryWork.py
+++ celeryWork.py
@@ -1,7 +1,15 @@
+import datetime
 from celery import Celery
 
-app = Celery('task', broker ='pyamqp://guest@localhost//')
+app = Celery('celeryWork', broker ='pyamqp://guest@localhost//')
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, add.s(x=2, y=4), name='add every 10')
 
 @app.task
 def add(x, y):
+    print(x + y)
+    with open('test.txt', 'w') as f:
+        f.write(f'x + y = {x + y} {datetime.datetime.now()}')
     return x + y

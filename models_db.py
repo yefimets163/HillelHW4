ecabee15ca8eff8fcac2147512b8e08d2974713b
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2023-03-22 23:39:50 +0200

    Update models_db.py

--- models_db.py
+++ models_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float
 from al_db import Base
 
-class User(Base):
+class Currency(Base):
     __tablename__ = 'currency'
     id = Column(Integer, primary_key=True, unique=True)
     bank = Column(String(50))
@@ -9,7 +9,6 @@ class User(Base):
     dateExchange = Column(String(120))
     buyRate = Column(Float)
     sellRate = Column(Float)
-
 
     def __init__(self, bank, currency, dateExchange, buyRate, sellRate):
         self.bank = bank

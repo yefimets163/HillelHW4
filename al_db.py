5f8290f094b9527ddbf98db9d4c7b699bc322aaf
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2023-03-22 23:39:18 +0200

    Update al_db.py

--- al_db.py
+++ al_db.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///currency.db')
+engine = create_engine(f'postgresql+psycopg2://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST", "127.0.0.1")}:5432/mydatabase')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
